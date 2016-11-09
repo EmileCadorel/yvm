2b39f74e4db0b0434b3f83c4624888466afd6caf
Emile Cadorel <ecadorel@gmail.com>
2016-11-09 19:14:15 +0100

    les operateur c'est bon

--- source/code/Operator.d
+++ source/code/Operator.d
@@ -1,5 +1,6 @@
 module code.Operator;
 import code.Frame, code.Instruction, code.Expression;
+import std.stdio;
 
 class Operator : Instruction {
 
@@ -16,6 +17,38 @@ class Operator : Instruction {
     }
 
     override void execute (Frame frame) {
-	
+	if (this._type == "plus") this._simple!"+" ();
+	else if (this._type == "minus") this._simple!"-" ();
+	else if (this._type == "star") this._simple!"*" ();
+	else if (this._type == "div") this._simple!"/" ();
+	else if (this._type == "inf") this._test!"<" ();
     }
+
+
+    private void _simple (string op) () {
+	auto left = this._left.get (), right = this._right.get (), res = this._res.get();
+	switch (this._left.size()) {
+	case 1: *res = cast(byte) mixin ("*left" ~ op ~ " *right"); break;
+	case 2: *(cast(short*)res) = cast(short) mixin ("*(cast(short*)left)" ~ op ~ " *(cast(short*)right)"); break;
+	case 4: *(cast(int*)res) = cast(int) mixin ("*(cast(int*)left)" ~ op ~ " *(cast(int*)right)"); break;
+	case 8: *(cast(long*)res) = cast(long) mixin ("*(cast(long*)left)" ~ op ~ " *(cast(long*)right)"); break;
+	case -4: *(cast(float*)res) = cast(float) mixin ("*(cast(float*)left)" ~ op ~ " *(cast(float*)right)"); break;
+	case -8: *(cast(double*)res) = cast(double) mixin ("*(cast(double*)left)" ~ op ~ " *(cast(double*)right)"); break;
+	default: break;
+	}
+    }
+	
+    private void _test (string op) () {
+	auto left = this._left.get (), right = this._right.get (), res = this._res.get();
+	switch (this._left.size()) {
+	case 1: *res = mixin ("*left" ~ op ~ " *right"); break;
+	case 2: *(res) =  mixin ("*(cast(short*)left)" ~ op ~ " *(cast(short*)right)"); break;
+	case 4: *(res) = mixin ("*(cast(int*)left)" ~ op ~ " *(cast(int*)right)"); break;
+	case 8: *(res) =  mixin ("*(cast(long*)left)" ~ op ~ " *(cast(long*)right)"); break;
+	case -4: *(res) =  mixin ("*(cast(float*)left)" ~ op ~ " *(cast(float*)right)"); break;
+	case -8: *(res) = mixin ("*(cast(double*)left)" ~ op ~ " *(cast(double*)right)"); break;
+	default: break;
+	}
+    }
+    
 }
